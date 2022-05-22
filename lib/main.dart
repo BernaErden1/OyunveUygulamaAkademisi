96a1e2f09acb141f47d02eaec976484532fe1c49
Zeynep Harputlugil <harputlugilzeynep@gmail.com>
2022-05-22 23:17:10 +0300

    Onboarding ekranları yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tarifim/giris_yap/giris_yap.dart';
 import 'package:tarifim/kayit_ol/kayit_ol.dart';
+import 'package:tarifim/onboarding/onboarding1.dart';
+import 'package:tarifim/onboarding/onboarding2.dart';
+import 'package:tarifim/onboarding/onboarding3.dart';
 
 import 'package:tarifim/product/tema/dark_tema.dart';
 import 'package:tarifim/product/tema/light_tema.dart';
@@ -20,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Mutfakta Neler Var ?',
       theme: LightTheme(context).lightTema,
       darkTheme: DarkTheme().darkTema,
-      home: const GirisYap(),
+      home: const Onboarding3(),
     );
   }
 }
---
+++ lib/onboarding/onboarding3.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:tarifim/product/dil/turkce_itemler.dart';
+import 'package:tarifim/product/utility.dart';
+
+class Onboarding3 extends StatelessWidget {
+  const Onboarding3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _heightone = MediaQuery.of(context).size.height * 0.7;
+    final _heighttwo = MediaQuery.of(context).size.height * 0.3;
+    final _width = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Column(
+                children: [
+                  SizedBox(
+                    height: _heightone,
+                    width: _width,
+                    child: Image.asset(
+                      "assets/onboarding3.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Container(
+                    height: _heighttwo,
+                    color: ColorsUtility().primaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(
+                        children: [
+                          HeadText(
+                            text: TurkceItemler().onboard3Headtext,
+                          ),
+                          SubText(text: TurkceItemler().onboard3subtext),
+                          spaceSize(),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              OnboardingDot(
+                                color: ColorsUtility().backgroundColor,
+                              ),
+                              OnboardingDot(
+                                color: ColorsUtility().backgroundColor,
+                              ),
+                              OnboardingDot(
+                                color: ColorsUtility().thirdColor,
+                              ),
+                            ],
+                          ),
+                          spaceSize(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon:
+                                    const Icon(Icons.navigate_before_outlined),
+                                iconSize: 60,
+                                color: ColorsUtility().thirdColor,
+                              ),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Başlayalım",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline5!
+                                        .copyWith(color: Colors.white),
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
