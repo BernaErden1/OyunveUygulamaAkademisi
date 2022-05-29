f48f1877f83ea313c86382de56630ea2d42729aa
Hava Israpilova <hava.isrplv@gmail.com>
2022-05-30 02:14:38 +0300

    Tarif kartları data dosyası hazırlandı.

---
+++ lib/product/tarif_kartlari_data.dart
@@ -0,0 +1,25 @@
+import 'package:tarifim/product/mock_data.dart';
+
+class TarifKartlari {
+  final String image;
+  final String time;
+  final String title;
+  final String subtitle;
+
+  TarifKartlari(this.image, this.time, this.title, this.subtitle);
+
+}
+
+List<TarifKartlari> tarifKartlariListesi = [
+  TarifKartlari(mock1.image, mock1.time, mock1.title, mock1.subtitle),
+  TarifKartlari(mock2.image, mock2.time, mock2.title, mock2.subtitle),
+  TarifKartlari(mock3.image, mock3.time, mock3.title, mock3.subtitle),
+  TarifKartlari(mock4.image, mock4.time, mock4.title, mock4.subtitle),
+  TarifKartlari(mock5.image, mock5.time, mock5.title, mock5.subtitle),
+  TarifKartlari(mock6.image, mock6.time, mock6.title, mock6.subtitle),
+  TarifKartlari(mock7.image, mock7.time, mock7.title, mock7.subtitle),
+  TarifKartlari(mock8.image, mock8.time, mock8.title, mock8.subtitle),
+  TarifKartlari(mock9.image, mock9.time, mock9.title, mock9.subtitle),
+  TarifKartlari(mock10.image, mock10.time, mock10.title, mock10.subtitle),
+  TarifKartlari(mock11.image, mock11.time, mock11.title, mock11.subtitle),
+];
